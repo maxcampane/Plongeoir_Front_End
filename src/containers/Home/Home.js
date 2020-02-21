57b848ca89b09fab261355d5ff77ddc58dc1bc80
Maxime Campane <mcampane@cardiweb.com>
2020-02-21 19:00:14 +0100

    Ajout page d'inscription

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -17,15 +17,37 @@ const styles = theme => ({
 });
 
 class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            check: true,
+        }
+    }
+
+    hideCard = () => {
+        this.setState({
+            check: !this.state.check
+        });
+    };
+
     render(){
         const { classes } = this.props;
+        console.log("check : " + this.state.check);
+        const displayMode = this.state.check ? "" : "hidden";
+        const backgroundColor = this.state.check ? "red" : "blue";
 
         return (
             <>
+                <div style={{backgroundColor: backgroundColor}}>
+                    Hello World
+                </div>
                 <Grid container spacing={3}>
-                    <Grid item sm={3} xs={12}>
-                        <Paper className={classes.paper}>xs=12 sm=6</Paper>
-                    </Grid>
+                    <div style={{display: displayMode, backgroundColor: backgroundColor}}>
+                        <Grid item sm={3} xs={12}>
+                            <Paper className={classes.paper}>xs=12 sm=6</Paper>
+                        </Grid>
+                    </div>
+                    <button onClick={this.hideCard}>click</button>
                     <Grid item sm={4} xs={12}>
                         <Paper className={classes.paper}>xs=12 sm=4</Paper>
                     </Grid>
